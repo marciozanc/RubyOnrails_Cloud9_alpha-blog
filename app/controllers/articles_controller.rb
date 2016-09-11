d0477844da58044c4e781b7ea4ebfbeb2019ecdc
alpha-blog <marciozanc@gmail.com>
2016-09-11 10:30:20 +0000

    Added Partial and  Destroy feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,11 +31,17 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
-  
   end
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was sucessfully deleted!!"
+    redirect_to articles_path
   end
 
   private
